fcac17ad40ff7abc2556da0fb041986997a1a34a
Prabin Swain <prabinswain027@gmail.com>
2026-09-07 01:19:55 +0530

    initial commit

---
+++ src/main/java/in/mortgage_admindao/mortgage_API/MortgageApiApplication.java
@@ -0,0 +1,13 @@
+package in.mortgage_admindao.mortgage_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/mortgage_admindao/mortgage_API/MortgageApiApplicationTests.java
@@ -0,0 +1,13 @@
+//package in.mortgage_admindao.mortgage_API;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MortgageApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
